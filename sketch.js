aa2429090217dad80d24cb387538cbd88294f78b
annettewhitehat <78954185+annettewhitehat@users.noreply.github.com>
2021-08-08 12:44:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var form, game, player;
+var database, position;
+var gameState = 0;
+var playerCount;
+var allPlayers;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(400,400);
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+   if(playerCount === 4){
+       game.update(1)
+   }
+   if(gameState === 1){
+       clear();
+       game.play();
+    
+   }
+}
+
